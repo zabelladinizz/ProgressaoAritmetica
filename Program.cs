5ce1f9921410b699928ed594bc73e540b9b2883d
Paulo Santos <isabelladiniz721@gmail.com>
2026-06-05 00:19:33 -0300

    aritmetica

---
+++ Program.cs
@@ -0,0 +1,15 @@
+﻿//Programa de Progressão Aritmética feito por Isabella Diniz Pin 1°ano A
+Console.WriteLine("Bem vindo ao Programa de progressões aritméticas");
+Console.WriteLine("Qual o termo incial da sua PA?");
+int TermoInicial = int.Parse(Console.ReadLine());
+Console.WriteLine("Quantos termos sua PA deve ter?");
+int termos = int.Parse(Console.ReadLine());
+Console.WriteLine("Qual deve ser a razão da sua PA?");
+int r = int.Parse(Console.ReadLine());
+Console.WriteLine("");
+for (int i = 1; i <= termos; i++) {
+    int TermoAtual = TermoInicial + (i - 1) * r;
+    Console.Write($"{TermoAtual} ");
+}
+
+
